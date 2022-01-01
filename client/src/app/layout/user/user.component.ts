0c2c53c79dd42c0995caa3ef3ec70701d1eeb87a
dmaksiuta <shutle369@gmail.com>
2022-01-01 21:36:46 +0200

    updated profile page and variables

--- client/src/app/layout/user/user.component.ts
+++ client/src/app/layout/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { UserService } from '@services';
+import { Animation } from "src/app/core/animation"
+
 @Component({
 	selector: 'app-user',
 	templateUrl: './user.component.html',
-	styleUrls: ['./user.component.scss']
+	styleUrls: ['./user.component.scss'],
+	animations: [ Animation ]
 })
 export class UserComponent {
 	public navToggle: any = false;
 	constructor(public us: UserService){}
 }
-
