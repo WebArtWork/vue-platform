938b76b0d21661dca9929a4e18e140af0a01d7ba
Honchar Denys <crackeraki@gmail.com>
2022-08-30 14:38:50 +0300

    angular update

--- client/src/app/modules/input/input.component.ts
+++ client/src/app/modules/input/input.component.ts
@@ -5,14 +5,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 	templateUrl: './input.component.html',
 	styleUrls: ['./input.component.scss']
 })
-export class InputComponent{
-	@Input() wngModel: any;
-	@Input() label: any;
+export class InputComponent {
+	@Input() wngModel: unknown;
+	@Input() label: string;
 	@Input() name = 'name';
 	@Input() type = 'text';
-	@Input() placeholder: any;
-	@Input() disabled: any;
+	@Input() placeholder: string;
+	@Input() disabled: bool;
 	@Output() wngModelChange = new EventEmitter()
 
-	constructor() {}
+	constructor() { }
 }
