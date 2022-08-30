341afa4c1873d0d367274dfcbb663073c5497307
Arsen <arsen.danylenko@gmail.com>
2022-08-30 12:06:36 +0300

    checked that the design of the w-input module matches that of the w-forms component, added the w-input module to the sign-in and profile pages

--- client/src/app/pages/guest/sign/sign.module.ts
+++ client/src/app/pages/guest/sign/sign.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core';
 import { SignComponent } from './sign.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonModule } from 'src/app/modules/button/button.module';
+import { InputModule } from 'src/app/modules/input/input.module';
 
 const routes: Routes = [{
 	path: '',
@@ -11,7 +13,9 @@ const routes: Routes = [{
 @NgModule({
 	imports: [
 		RouterModule.forChild(routes),
-		CoreModule
+		CoreModule,
+		ButtonModule,
+		InputModule
 	],
 	declarations: [
 		SignComponent
