04c5bb9e012624b2916125846f6ce9eca6adea54
Arsen <arsen.danylenko@gmail.com>
2022-08-31 14:33:08 +0300

    commit modules

---
+++ client/src/app/modules/card1/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+@Component({
+	selector: 'wcard',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+	@Input() padding = false;
+	@Input() align = 'center';
+	@Input() image: boolean;
+	@Input() title: string;
+	@Input() subtitle: string;
+	@Input() text: string;
+
+	constructor() { }
+}
