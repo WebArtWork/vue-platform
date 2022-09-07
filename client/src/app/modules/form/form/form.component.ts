498abe5871380a0bd925b375192284121efc4e8c
Honchar Denys <crackeraki@gmail.com>
2022-09-07 19:16:07 +0300

    cleanup form component

--- client/src/app/modules/form/form/form.component.ts
+++ client/src/app/modules/form/form/form.component.ts
@@ -1,30 +1,14 @@
 import { Component, EventEmitter, Input, Output  } from '@angular/core';
-import { UiService } from 'wacom';
-import { ButtonTypes } from '../../button/button.interface';
-import { InputTypes } from '../../input/input.interface';
 import {
 	FormConfig,
-	FormComponent as FormComponentInterface,
-	FormModules,
-	FormOutputs,
-	FormService
+	FormComponent as FormComponentInterface
 } from '../form.service';
-interface Any {
-	[key: string]: boolean;
-}
 @Component({
 	selector: 'wform',
 	templateUrl: './form.component.html',
 	styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-	readonly modules = FormModules;
-
-	readonly inputTypes = InputTypes;
-
-	readonly buttonTypes = ButtonTypes;
-
-	readonly outputs = FormOutputs;
 
 	@Input() config: FormConfig;
 
@@ -33,11 +17,6 @@ export class FormComponent {
 	@Output() submit = new EventEmitter();
 
 	@Output() change = new EventEmitter();
-
-	constructor(
-		public fm: FormService,
-		public ui: UiService
-	) {}
 
 	focus(component: FormComponentInterface): void {
 		console.log(component);
