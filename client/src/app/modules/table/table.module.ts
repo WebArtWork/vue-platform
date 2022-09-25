454c9741fdbd542b9ce46add7b204e5744a6d717
Kondruk <kondrukroman@gmail.com>
2022-09-25 16:50:46 +0300

    fixed ng-template issue

--- client/src/app/modules/table/table.module.ts
+++ client/src/app/modules/table/table.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { WacomModule } from 'wacom';
+import { ActionsDirective, CellDirective, CustomEditDirective, SortDirective } from './table.directive';
 
 @NgModule({
 	imports: [CommonModule, FormsModule, WacomModule, FormModule],
-	declarations: [TableComponent],
+	declarations: [TableComponent, CellDirective, SortDirective, ActionsDirective, CustomEditDirective],
 	providers: [],
-	exports: [TableComponent]
+	exports: [TableComponent, CellDirective, SortDirective, ActionsDirective, CustomEditDirective]
 })
 export class TableModule {}
