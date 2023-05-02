145c6d4b24bab5a9d8759626187a46d726a130bc
Honchar Denys <crackeraki@gmail.com>
2023-05-03 01:01:58 +0300

    latest fixes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,13 +8,19 @@ import { TranslateService } from './modules/translate/translate.service';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-	@ViewChild('textRef', { static: true }) textRef: TemplateRef<any>;
-	@ViewChild('dateRef', { static: true }) dateRef: TemplateRef<any>;
-	@ViewChild('timeRef', { static: true }) timeRef: TemplateRef<any>;
-	@ViewChild('numberRef', { static: true }) numberRef: TemplateRef<any>;
-	@ViewChild('emailRef', { static: true }) emailRef: TemplateRef<any>;
-	@ViewChild('passwordRef', { static: true }) passwordRef: TemplateRef<any>;
-	@ViewChild('buttonRef', { static: true }) buttonRef: TemplateRef<any>;
+	@ViewChild('textRef', { static: true }) textRef: TemplateRef<unknown>;
+
+	@ViewChild('dateRef', { static: true }) dateRef: TemplateRef<unknown>;
+
+	@ViewChild('timeRef', { static: true }) timeRef: TemplateRef<unknown>;
+
+	@ViewChild('numberRef', { static: true }) numberRef: TemplateRef<unknown>;
+
+	@ViewChild('emailRef', { static: true }) emailRef: TemplateRef<unknown>;
+
+	@ViewChild('passwordRef', { static: true }) passwordRef: TemplateRef<unknown>;
+
+	@ViewChild('buttonRef', { static: true }) buttonRef: TemplateRef<unknown>;
 
 	ngOnInit(): void {
 		this._form.addComponent({
@@ -22,36 +28,43 @@ export class AppComponent implements OnInit {
 			ref: this.textRef,
 			fields: ['Placeholder', 'Label']
 		});
+
 		this._form.addComponent({
 			name: 'Date',
 			ref: this.dateRef,
 			fields: ['Placeholder', 'Label']
 		});
+
 		this._form.addComponent({
 			name: 'Time',
 			ref: this.timeRef,
 			fields: ['Placeholder', 'Label']
 		});
+
 		this._form.addComponent({
 			name: 'Number',
 			ref: this.numberRef,
 			fields: ['Placeholder', 'Label']
 		});
+
 		this._form.addComponent({
 			name: 'Email',
 			ref: this.emailRef,
 			fields: ['Placeholder', 'Label']
 		});
+
 		this._form.addComponent({
 			name: 'Password',
 			ref: this.passwordRef,
 			fields: ['Placeholder', 'Label']
 		});
+
 		this._form.addComponent({
 			name: 'Button',
 			ref: this.buttonRef,
 			fields: ['Label']
 		});
+
 		this._form.inited = true;
 	}
 
@@ -59,7 +72,9 @@ export class AppComponent implements OnInit {
 		private _form: FormService,
 		private _translate: TranslateService
 	) {
-		this._form.setTranslate(this._translate.translate.bind(this._translate));
+		this._form.setTranslate(
+			this._translate.translate.bind(this._translate)
+		);
 
 		this._form.addForm({
 			formId: 'sign',
@@ -123,6 +138,7 @@ export class AppComponent implements OnInit {
 				}
 			]
 		});
+
 		this._form.addForm({
 			formId: 'change password',
 			title: 'Change password',
@@ -157,18 +173,10 @@ export class AppComponent implements OnInit {
 							value: 'New Password'
 						}
 					]
-				},
-				{
-					name: 'Button',
-					fields: [
-						{
-							name: 'Label',
-							value: 'Change'
-						}
-					]
 				}
 			]
 		});
+
 		this._form.addForm({
 			formId: 'profile',
 			title: 'Profile Settings',
