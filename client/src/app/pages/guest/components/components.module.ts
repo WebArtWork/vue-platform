df9b4187426f7608ba5f5ebe2206069035897e5f
Honchar Denys <crackeraki@gmail.com>
2024-09-22 23:51:02 +0300

    added some components

--- client/src/app/pages/guest/components/components.module.ts
+++ client/src/app/pages/guest/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core/core.module';
 import { ComponentsComponent } from './components.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FileModule } from 'src/app/core/modules/file/file.module';
 
 const routes: Routes = [
 	{
@@ -11,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forChild(routes), CoreModule],
+	imports: [RouterModule.forChild(routes), CoreModule, FileModule],
 	declarations: [ComponentsComponent],
 	providers: []
 })
