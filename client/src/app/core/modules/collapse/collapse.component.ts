92dcc73d91777f7ec3c283a93a4c6c726f02975e
lizvinskyi <andriilizvinskyi@gmail.com>
2024-10-17 12:18:59 +0300

    colapse fixes

--- client/src/app/core/modules/collapse/collapse.component.ts
+++ client/src/app/core/modules/collapse/collapse.component.ts
@@ -9,6 +9,17 @@ import { Collapse } from './collapse.interface';
 export class CollapseComponent {
 	@Input() config: Collapse;
 
+	constructor() {
+		this.config = this.config || ({} as Collapse);
+
+		this.config.show =
+			typeof this.config.show === 'boolean' ? this.config.show : false; 
+
+		this.config.toggle = this.config.toggle || this.toggle.bind(this);
+		this.config.open = this.config.open || this.open.bind(this);
+		this.config.close = this.config.close || this.close.bind(this);
+	}
+
 	toggle(): void {
 		this.config.show = !this.config.show;
 	}
@@ -19,18 +30,5 @@ export class CollapseComponent {
 
 	close(): void {
 		this.config.show = false;
-	}
-
-	constructor() {
-		this.config = this.config || ({} as Collapse);
-
-		this.config.show =
-			typeof this.config.show === 'boolean' ? this.config.show : true;
-
-		this.config.toggle = this.config.toggle || this.toggle.bind(this);
-
-		this.config.open = this.config.open || this.open.bind(this);
-
-		this.config.close = this.config.close || this.close.bind(this);
 	}
 }
